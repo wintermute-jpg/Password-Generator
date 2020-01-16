c4cbc9be3fb380f0a30218f974dd67d6f0591757
Gabe Young <gabriel_young@emerson.edu>
2020-01-16 14:23:50 -0500

    finalized java + HTML

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ var special= "!~@#$%^&*()_+<>?/.,=-`/";
 var digits = "123456789";
 var passwordContents = "";
 
+
+
 function generate(){
     let password = "";  
     var userLength = prompt("Please specify a length between 8-128 characters");
     if (userLength <8 || userLength > 128) {
         alert("Please re-enter a value between 8 and 128");
-        userLength = prompt("Please specify a length between 8-128 characters");
+        generate();
     } else 
     {var specialChar = confirm("Would you like to use special characters?");
     var numbers = confirm("Would you like to include numbers?");
@@ -24,25 +26,28 @@ function generate(){
         passwordContents += digits;
     }
 
-    if(lowerCase === true) {
+    if(lowerCase === true) { 
         passwordContents += lower;
     }
 
     if(upperCase === true) {
-        passwordContests += upper;
+        passwordContents += upper;
     }
     
-    console.log(passwordContents);
-
     for(var i=0; i<=userLength; i++){
-        password.length = userLength.valueOf;
+       password = password + passwordContents.charAt(Math.floor(Math.random()*Math.floor(passwordContents.length -1)));
     }
+
+    console.log(password);
+
+    document.getElementById("passwordSec").value = password;
 }
 }
 
 function copy(){
-    document.getElementById("display").select();
-    document.execCommand("Copy");
-    alert("You password has been copied!! Smashing!")
+    var copyText = document.getElementById("password");
+    document.getElementById("passwordSec").select();
+    document.execCommand("copy");
+    alert("You password has been copied!! Smashing!");
 }
 
